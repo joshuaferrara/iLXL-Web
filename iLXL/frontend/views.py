ede4f20fd4e9e7b6f4f4aaa709c1017115445e97
Dayna-A <dayna.anderson@csu.fullerton.edu>
2018-12-01 20:55:50 -0800

    broken home

--- iLXL/frontend/views.py
+++ iLXL/frontend/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html', {})
+    return render(request, 'templates/base.html', {})
